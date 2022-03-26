f51a7472c8484975bbbd0a951c4c49a9288cf3fc
Oleg <mozgarik1613@gmail.com>
2022-03-26 20:17:13 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const body = document.body 
+let timeID = null;
 
+startBtn.addEventListener('click', startChangeColor)
+stopBtn.addEventListener('click', stopChangeColor)
+
+function startChangeColor() {
+startChangeHexColor();
+toggleButton();
+}
+
+function stopChangeColor() {
+stopChangeHexColor();
+}
+
+function startChangeHexColor() {
+    timeID = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopChangeHexColor() {
+clearInterval(timeID);
+toggleButton();
+}
+
+function toggleButton() {
+    startBtn.toggleAttribute('disabled')
+    stopBtn.toggleAttribute('disabled')
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
